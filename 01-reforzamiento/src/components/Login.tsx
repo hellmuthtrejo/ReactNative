32104ec8dfa5f8b31aef9f57097bedaf6650dafb
hellmuth trejo <htrejo@inventa.cl>
2021-12-07 15:40:27 -0300

    useReducer

--- 01-reforzamiento/src/components/Login.tsx
+++ 01-reforzamiento/src/components/Login.tsx
@@ -1,14 +1,68 @@
+import { useReducer, useEffect } from 'react';
+
+interface AuthState {
+    validando: boolean;
+    token: (string | null);
+    username: string;
+    nombre: string
+}
+
+const initialState: AuthState = {
+    validando: true,
+    token: null,
+    username: '',
+    nombre: '',
+}
+
+type AuthAction = {
+    type: 'logout'
+}
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+    switch (action.type) {
+        case 'logout':
+            return {
+                validando: false,
+                token: null,
+                username: '',
+                nombre: ''
+            }
+
+        default:
+            return state;
+    }
+}
+
 export const Login = () => {
+    
+    // const [state, dispatch] = useReducer(authReducer, initialState);
+    const [{validando}, dispatch] = useReducer(authReducer, initialState);
+
+    useEffect(() => {
+        setTimeout(() => {
+            dispatch({ type: 'logout'});
+        }, 1500);
+    }, []);
+
+    if( validando ) {
+        return (
+            <>
+                <h3>Login</h3>
+                <div className="alert alert-info">
+                    Validando...
+                </div>
+            </>
+        );
+    }
+ 
     return (
         <>
             <h3>Login</h3>
 
-            <div className="alert alert-info">
-                Validando...
-            </div>
             <div className="alert alert-danger">
                 No autenticado
             </div>
+
             <div className="alert alert-success">
                 Autenticado
             </div>
